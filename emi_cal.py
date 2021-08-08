9a60c873e3adbf1b01b0d3bf7a52b30b2490623c
Samarth2202 <88436873+Samarth2202@users.noreply.github.com>
2021-08-08 11:09:47 +0530

    Add files via upload

--- emi_cal.py
+++ emi_cal.py
@@ -4,3 +4,5 @@ def calculate_emi(p, n, r):
   return p*(r/100)*((1+(r/100)**n)/((1+(r/100)**n) - 1))
 
 st.title("EMI Calaculator")
+principal = st.sidebar.slider('Principal Loan Amount',1000,100000)
+tenure = st.sidebar.slider('Loan Period(in years)',1,30)
