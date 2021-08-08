07188dfb8f6faa0fe503f0ddf50971b826da0292
Samarth2202 <88436873+Samarth2202@users.noreply.github.com>
2021-08-08 14:16:44 +0530

    Add files via upload

--- emi_cal.py
+++ emi_cal.py
@@ -1,7 +1,8 @@
+# Write the code for creating the EMI calculator app
 import streamlit as st
 @st.cache()
 def calculate_emi(p, n, r):
-  return p*(r/100)*((1+(r/100))**n/((1+(r/100))**n - 1))
+  return round(p*(r/100)*((1+(r/100))**n/((1+(r/100))**n - 1)),2)
 
 st.title('EMI Calaculator')
 principal = st.sidebar.slider('Principal Loan Amount',1000,100000)
