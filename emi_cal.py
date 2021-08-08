c01057ab4a8f103f399a46498ebdefd886845100
Samarth2202 <88436873+Samarth2202@users.noreply.github.com>
2021-08-08 11:07:13 +0530

    Add files via upload

--- emi_cal.py
+++ emi_cal.py
@@ -3,7 +3,7 @@ import streamlit as st
 def calculate_emi(p, n, r):
   return p*(r/100)*((1+(r/100)**n)/((1+(r/100)**n) - 1))
 
-st.title('EMI Calaculator')
+st.title("EMI Calaculator")
 principal = st.sidebar.slider('Principal Loan Amount',1000,100000)
 tenure = st.sidebar.slider('Loan Period(in years)',1,30)
 n = tenure*12
