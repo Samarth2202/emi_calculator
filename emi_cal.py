4c9145e13ca0c03238289e4da9ce3310bb146c8d
Samarth2202 <88436873+Samarth2202@users.noreply.github.com>
2021-08-08 10:55:37 +0530

    Add files via upload

--- emi_cal.py
+++ emi_cal.py
@@ -9,4 +9,4 @@ n = tenure*12
 roi = st.sidebar.slider('Rate of Interest(in % per annum)',1,15)
 r = roi/12
 if st.sidebar.bottom('Calculate EMI'):
-  'Your EMI is : ',calculate_emi(principal, n, r)
+  calculate_emi(principal, n, r)
